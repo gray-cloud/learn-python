097444c7269ee4c9ddd93e2362d51107eb4c1b13
XuXingLong <xxl20959@gmail.com>
2017-10-15 01:12:00 +0800

    Python基础
    Python基础数据类型

---
+++ python3/test1015-1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# print absolut value of an integer;
+a = 100
+if a>= 0:
+    print(a)
+else:
+    print(-a)
---
+++ python3/test1015-2.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+age = 20
+if age >= 18:
+    print('adult')
+else:
+    print('teenager')
