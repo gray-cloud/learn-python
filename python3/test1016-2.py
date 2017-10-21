8d876ef0accef9200aa059a855217582e5b746b4
XuXingLong <xxl20959@gmail.com>
2017-10-22 01:05:01 +0800

    list and tuple
    list列表跟tuple元祖

---
+++ python3/test1016-2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+s1 = 72
+s2 = 85
+
+r = (s2 - s1) / s1 * 100
+print('小明成绩提升为：%.1f %%' % r)
---
+++ python3/test1022-1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# _*_ coding: utf-8 _*_
+
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Lisa']
+]
+
+# Apple
+print(L[0][0])
+# Pyton
+print(L[1][1])
+# Lisa
+print(L[2][2])
